e53e33149a73d72f61abb0d25df9858fcb62223b
Azizbek Asadov <azizbek1998@icloud.com>
2022-10-28 13:43:33 +0500

    Initial Commit

---
+++ Fasting Timer/Fasting_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fasting_TimerApp.swift
+//  Fasting Timer
+//
+//  Created by Azizbek Asadov on 28/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Fasting_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
